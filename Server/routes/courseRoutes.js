c6ee8146c08870f07ee3d36dacadd46c438ee565
Rajan Kumar <raorajan9576@gmail.com>
2023-11-03 19:01:53 +0530

    folder structure created for frontend

--- Server/routes/courseRoutes.js
+++ Server/routes/courseRoutes.js
@@ -3,15 +3,16 @@ const courseRouter = express.Router();
 const CourseController = require('../controllers/CourseController');
 const {checkLoggedIn} = require('../middlewares/authenticationMiddleware');
 //post data
+
 courseRouter.post('/courses', CourseController.createCourse);
 // Retrieve a list of courses
 courseRouter.get('/courses',checkLoggedIn, CourseController.getCourses);
 // Retrieve details of a specific course
 courseRouter.get('/courses/:courseId', CourseController.getCourseDetails);
 // Mark a course as completed for a student
-courseRouter.put('/courses/:courseId/completed', CourseController.markCourseAsCompleted);
+courseRouter.put('/courses/:courseId/completed',checkLoggedIn, CourseController.markCourseAsCompleted);
 // Retrieve course details for the student's dashboard
-courseRouter.get('/students/:studentId/courses/:courseId/details', CourseController.getCourseDetailsForDashboard);
+courseRouter.get('/students/:studentId/courses/:courseId/details',checkLoggedIn, CourseController.getCourseDetailsForDashboard);
 // Define a route for paginated course list
 courseRouter.get('/coursesperpage', CourseController.getPaginatedCourses);
 //search by 
