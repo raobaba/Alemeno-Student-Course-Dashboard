842bcd0f16c7b15a8a76961d00f6a407cea1e3a1
Rajan Kumar <raorajan9576@gmail.com>
2023-11-03 19:15:26 +0530

    CouseDetails Page Created

---
+++ Client/src/components/CourseDetails.jsx
@@ -0,0 +1,30 @@
+// CourseDetails.js
+
+import React from 'react';
+import '../styles/CourseDetails.css'; // Import your CSS file for styling.
+
+function CourseDetails({ course }) {
+  return (
+    <div className="course-details">
+      <h1>Course Details</h1>
+      <div className="details-container">
+        <div className="course-info">
+          <h2>{course.name}</h2>
+          <p>Instructor: {course.instructor}</p>
+          <p>Description: {course.description}</p>
+          <p>Enrollment Status: {course.status}</p>
+          <p>Course Duration: {course.duration}</p>
+          <p>Schedule: {course.schedule}</p>
+          <p>Location: {course.location}</p>
+          <p>Pre-requisites: {course.prerequisites}</p>
+        </div>
+        <div className="syllabus">
+          <h3>Syllabus</h3>
+          <p>{course.syllabus}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CourseDetails;
