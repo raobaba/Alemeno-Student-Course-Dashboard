f385eca2f465dc4d364db11f1d5120703b6cf84b
Rajan Kumar <raorajan9576@gmail.com>
2023-11-03 19:20:21 +0530

    StudentDashboard page created

--- Client/src/components/StudentDashboard.jsx
+++ Client/src/components/StudentDashboard.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+// StudentDashboard.js
+import React, { useState } from 'react';
+import '../styles/StudentDashboard.css'; // Import your CSS file for styling.
 
-function StudentDashboard() {
+function StudentDashboard({ enrolledCourses }) {
+  const [completedCourses, setCompletedCourses] = useState([]);
+
+  const markAsCompleted = (courseId) => {
+    setCompletedCourses((prevCourses) => [...prevCourses, courseId]);
+  };
+
   return (
-    <div>StudentDashboard</div>
-  )
+    <div className="student-dashboard">
+      <h1>Student Dashboard</h1>
+      <div className="enrolled-courses">
+        {enrolledCourses.map((course) => (
+          <div key={course.id} className="course-card">
+            <img src={course.thumbnail} alt={course.name} />
+            <div className="course-details">
+              <h2>{course.name}</h2>
+              <p>Instructor: {course.instructor}</p>
+              <p>Due Date: {course.dueDate}</p>
+              <div className="progress-bar">
+                <div
+                  className={`progress ${completedCourses.includes(course.id) ? 'completed' : ''}`}
+                />
+              </div>
+              <button
+                onClick={() => markAsCompleted(course.id)}
+                disabled={completedCourses.includes(course.id)}
+              >
+                Mark as Completed
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default StudentDashboard
+export default StudentDashboard;
