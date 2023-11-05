20933b1cd974a8025718db1d6626c47afbf01ae2
Rajan Kumar <raorajan9576@gmail.com>
2023-11-05 21:39:34 +0530

    correct till now

--- Client/src/redux/actions/CourseAction.js
+++ Client/src/redux/actions/CourseAction.js
@@ -8,26 +8,14 @@ import {
   FETCH_COURSE_LOADING,
   FETCH_COURSE_SUCCESS,
   FETCH_COURSE_FAILURE,
-  SET_PAGINATION, // Include the SET_PAGINATION action type
 } from '../actionTypes/CourseActionType';
 
-// Add a new action creator for setting pagination
-export const setPagination = (page, pageSize) => {
-  return {
-    type: SET_PAGINATION,
-    payload: { page, pageSize },
-  };
-};
-
-// CourseActions.js
-export const fetchCourses = (page, pageSize) => {
+export const fetchCourses = () => {
   return (dispatch) => {
-    dispatch(setPagination(page, pageSize)); // Set pagination data
-
     dispatch({ type: FETCH_COURSES_LOADING });
 
     api
-      .get('/coursesperpage', { params: { page, pageSize } }) // Include pagination parameters in the request
+      .get('/courses') // Use the correct API endpoint
       .then((response) => {
         dispatch({
           type: FETCH_COURSES_SUCCESS,
@@ -64,3 +52,5 @@ export const fetchCourse = (courseId) => {
       });
   };
 };
+
+
